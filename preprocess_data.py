e611d47fc80296f9491a1ac559b091d0bd155aee
Andreas <andreashi2@gmail.com>
2019-01-03 21:58:36 +0100

    Improved data generation

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,16 +1,20 @@
+import random
 import re
-pattern = re.compile("(^[a-zA-Z0-9_-åøæ ]{40,150}\\.)")
 
 count = 0
 with open("raw_data/wiki.no.txt", encoding="utf-8") as f:
-    with open('data.txt', 'a') as the_file:
+    with open('data.txt', 'w') as the_file:
         for e in f:
-            match = pattern.match(e)
+            max = random.randint(5, 50)
+            pattern = re.compile("( [a-zA-Z0-9_\\-åøæ ,]{2,"+str(max)+"}[.?!])")
+            match = pattern.findall(e)
             if match:
                 #print(match.groups())
-                for match in match.groups():
-                    the_file.write(match + "\n")
-                    if len(match) > 199:
+                for match in match:
+                    the_file.write(match.lstrip() + "\n")
+                    if 'µ' in match:
+                        print(match)
+                    if len(match) > 150:
                         print("big sentence: ", match)
                     count += 1
 
